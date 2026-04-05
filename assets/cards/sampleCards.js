6026b8436c525498e3b07b65060829bda0469ca7
CC3031 <acunafernando617@gmail.com>
2026-04-05 11:31:53 -0500

    added css to make cards look good and rows responsive

--- assets/cards/sampleCards.js
+++ assets/cards/sampleCards.js
@@ -37,6 +37,7 @@ sampleProducts.forEach(product => {
     const card = document.createElement("div");
     card.classList.add("product-card");
 
+
     card.innerHTML = `
         <img src="${product.image}" alt="${product.name}">
         <h3>${product.name}</h3>
